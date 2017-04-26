4bc78d0ce2053b9820034057816616dcf4ae2020
Mooba <Mombasa3d@gmail.com>
2017-04-25 22:08:01 -0600

    FlashCard update

---
+++ FlashCards/FlashCards/FlashCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards
+{
+    class FlashCard
+    {
+        private string Keyword { get; set; }
+        private string Definition { get; set; }
+
+        public FlashCard(string key, string def)
+        {
+            Keyword = key;
+            Definition = def;
+        }
+    }
+}
---
+++ FlashCards/FlashCards/FlashCardMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleIO;
+
+namespace FlashCards
+{
+    class FlashCardMain
+    {
+        private static Random rando = new Random();
+        private static string[] manageList = new string[] { "Add card", "Remove card", "Save cards", "Main Menu" };
+        private static List<string> menuList = new List<string> { "Manage Flash Cards", "Review", "Exit" };
+        private static Dictionary<string, FlashCard> cardBank = new Dictionary<string, FlashCard>();
+        private static List<string> hashKeys = new List<string>();
+
+        public static void FlashCardPrompt()
+        {
+            int menuChoice = ConsoleIO.CIO.PromptForMenuSelection(menuList, true);
+
+            switch(menuChoice)
+            {
+                case 1:
+                    ManageMenu(menuChoice);
+                    break;
+                case 2:
+                    Review();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void Review()
+        {
+
+        }
+
+        private static void ManageMenu()
+        {
+            ConsoleIO.CIO.PromptForMenuSelection(manageList, false);
+            switch()
+        }
+    }
+}
